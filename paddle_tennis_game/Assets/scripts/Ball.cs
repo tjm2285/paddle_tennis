1d3cc08425261c211a35656ee60c0c044199cb7e
tjm2285 <tjm2285@yahoo.com>
2024-03-06 11:32:26 -0800

    added ball bounce off game area bounds. added paddle movement logic

--- paddle_tennis_game/Assets/scripts/Ball.cs
+++ paddle_tennis_game/Assets/scripts/Ball.cs
@@ -7,9 +7,14 @@ public class Ball : MonoBehaviour
     [SerializeField, Min(0f)]
     float
         constantXSpeed = 8f,
-        constantYSpeed = 10f;
+        constantYSpeed = 10f,
+		extents = 0.5f;
 
     Vector2 position, velocity;
+
+    public float Extents => extents;
+
+    public Vector2 Position => position;
 
     public void UpdateVisualization() =>
         transform.localPosition = new Vector3(position.x, 0f, position.y);
@@ -22,6 +27,19 @@ public class Ball : MonoBehaviour
         UpdateVisualization();
         velocity = new Vector2(constantXSpeed, -constantYSpeed);
     }
+
+    public void BounceX(float boundary)
+    {
+        position.x = 2f * boundary - position.x;
+        velocity.x = -velocity.x;
+    }
+
+    public void BounceY(float boundary)
+    {
+        position.y = 2f * boundary - position.y;
+        velocity.y = -velocity.y;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- paddle_tennis_game/Assets/scripts/GameManager.cs
+++ paddle_tennis_game/Assets/scripts/GameManager.cs
@@ -11,11 +11,44 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     Paddle bottomPaddle, topPaddle;
 
+    [SerializeField, Min(0f)]
+    Vector2 arenaExtents = new Vector2(10f, 10f);
+
     void Awake() => ball.StartNewGame();
 
     void Update()
     {
+        bottomPaddle.Move(ball.Position.x, arenaExtents.x);
+        topPaddle.Move(ball.Position.x, arenaExtents.x);
         ball.Move();
+        BounceYIfNeeded();
+        BounceXIfNeeded();
         ball.UpdateVisualization();
     }
+
+    void BounceYIfNeeded()
+    {
+        float yExtents = arenaExtents.y - ball.Extents;
+        if (ball.Position.y < -yExtents)
+        {
+            ball.BounceY(-yExtents);
+        }
+        else if (ball.Position.y > yExtents)
+        {
+            ball.BounceY(yExtents);
+        }
+    }
+    void BounceXIfNeeded()
+    {
+        float xExtents = arenaExtents.x - ball.Extents;
+        if (ball.Position.x < -xExtents)
+        {
+            ball.BounceX(-xExtents);
+        }
+        else if (ball.Position.x > xExtents)
+        {
+            ball.BounceX(xExtents);
+        }
+    }
+    
 }
