ab823ae962ff36719823356b12a1ecd817e57f9a
tjm2285 <tjm2285@yahoo.com>
2024-03-06 09:48:31 -0800

    added basic scripts. moved main camera

---
+++ paddle_tennis_game/Assets/scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.VirtualTexturing.Debugging;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    Ball ball;
+
+    [SerializeField]
+    Paddle bottomPaddle, topPaddle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
