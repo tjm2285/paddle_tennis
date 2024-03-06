6b9f09799caed47fa85594ae619fa2099d36031d
tjm2285 <tjm2285@yahoo.com>
2024-03-06 13:44:13 -0800

    added reactive movement to camera

---
+++ paddle_tennis_game/Assets/scripts/LivelyCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivelyCamera : MonoBehaviour
+{
+    [SerializeField, Min(0f)]
+    float
+     jostleStrength = 40f,
+     pushStrength = 1f;
+
+    Vector3 velocity;
+
+    public void JostleY() => velocity.y += jostleStrength;
+
+    public void PushXZ(Vector2 impulse)
+    {
+        velocity.x += pushStrength * impulse.x;
+        velocity.z += pushStrength * impulse.y;
+    }
+
+    void LateUpdate()
+    {
+        transform.localPosition += velocity * Time.deltaTime;
+    }
+}
